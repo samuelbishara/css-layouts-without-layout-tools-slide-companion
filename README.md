e457b5faf6133a44d87a37fc012d9fafe2c3d6e8
Don Hamilton III <donhamiltoniii@gmail.com>
2018-10-05 14:55:28 -0400

    Add Spring Boot Backend

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # CSS Layouts Without Layout Tools
 
 This example is a companion to [this](http://wecancodeit.github.io/java-slides/frontend/css-layouts-without-layout-tools) slide deck.
+
+## Installation Instructions
+
+1. Clone this project into `wcci/code`
+1. `cd` into this project directory
+1. `gradle eclipse`
+1. Pending a Successful Build, import into Eclipse
+1. Start the Application and navigate to `localhost:8080` in your browser
---
+++ src/main/java/org/wecancodeit/csslayoutswithoutlayouttoolsslidecompanion/CssLayoutsWithoutLayoutToolsSlideCompanionApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.csslayoutswithoutlayouttoolsslidecompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssLayoutsWithoutLayoutToolsSlideCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssLayoutsWithoutLayoutToolsSlideCompanionApplication.class, args);
+	}
+}
